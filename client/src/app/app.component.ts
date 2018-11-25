d2750a5e66842d43a9aed43de50cba218f93b092
pankaj_119 <pankaj.7613@gmail.com>
2018-11-25 15:34:23 +0530

    done

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,12 +10,37 @@ export class AppComponent implements OnInit {
   private products: any[];
   product: any = {};
   p: number = 1;
+  btn_stt : boolean = false;
+  update_id : string;
+  alert_stt : boolean = false;
   constructor(private http: Http, private data: MessageService) {
 
   }
 
   ngOnInit() {
     this.getProucts();
+  }
+
+  editProduct(product){
+    this.alert_stt = false;
+    this.update_id = product._id;
+    this.btn_stt = true;
+    this.product = product;
+  }
+
+  deleteProduct(id){
+    this.alert_stt = false;
+    if(confirm("Do you want to delete?")){
+      this.http.delete("http://localhost:3000/api/products/"+id).subscribe(
+      (res) => {
+       this.data.success("Record deleted successfully!");
+       this.alert_stt = true;
+       this.getProucts();
+      },
+      (err) => {
+        console.log('The error is ', err)
+      })
+    }
   }
 
   getProucts() {
@@ -29,13 +54,30 @@ export class AppComponent implements OnInit {
       })
   }
 
+  updateProduct(){
+    this.alert_stt = false;
+    if (this.validate()) {
+      this.http.put("http://localhost:3000/api/products/"+this.update_id, this.product).subscribe(
+        (res) => {
+          this.getProucts();
+          this.data.success("Product updated successfully!")
+          this.alert_stt = true;
+        },
+        (err) => {
+          console.log('The error is ', err)
+        })
+    }
+  }
+
   saveProduct() {
+   
     if (this.validate()) {
       this.http.post("http://localhost:3000/api/products", this.product).subscribe(
         (res) => {
           this.getProucts();
           this.product = {};
           this.data.success("Product saved successfully!")
+          this.alert_stt = true;
         },
         (err) => {
           console.log('The error is ', err)
